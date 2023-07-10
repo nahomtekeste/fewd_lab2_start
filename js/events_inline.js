640921434384ad86ebd2afe845af77fee03bbfcb
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2023-07-10 16:59:27 +0100

    Starter code for Lab2

--- js/events_inline.js
+++ js/events_inline.js
@@ -2,13 +2,11 @@
  function expand(divname)
  {
    let mydiv=document.getElementById(divname);
-   let currentdisplay = mydiv.style.display;
    mydiv.style.display = "block";    
  }
  
  function collapse(divname)
  {
    let mydiv=document.getElementById(divname);
-   let currentdisplay = mydiv.style.display;
    mydiv.style.display = "none";        
  }
